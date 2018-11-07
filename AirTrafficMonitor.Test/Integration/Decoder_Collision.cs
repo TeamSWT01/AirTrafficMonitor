324f743e54f20ca9367050369319299c199bd1e6
Mikkel <mikkel_busk@hotmail.com>
2018-11-07 23:13:08 +0100

    Added integration test for Decoder_Track and Decoder_Collision

---
+++ AirTrafficMonitor.Test/Integration/Decoder_Collision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using AirTrafficMonitor.Implementation;
+using AirTrafficMonitor.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AirTrafficMonitor.Test.Integration
+{
+    [TestFixture]
+    public class Decoder_Collision
+    {
+        private Decoder decoder;
+        private IWriter _writer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            decoder = new Decoder();
+            _writer = Substitute.For<IWriter>();
+            decoder.CollisionDetector.LogWriter = _writer;
+        }
+
+        [TestCase("ABC123", "10000", "10000", "1000", "20180427120717123")]
+        public void DecodeTransData_TrackIsInAirspace_ResultAsExpected(string tag, string x, string y, string altitude,
+            string dateTime)
+        {
+            List<string> list = new List<string>() { tag + ';' + x + ';' + y + ';' + altitude + ';' + dateTime,
+                                                     "XX1234" + ';' + x + ';' + y + ';' + altitude + ';' + dateTime };
+
+            decoder.DecodeTransData(new object(), new RawTransponderDataEventArgs(list));
+
+            _writer.Received().Write(Arg.Any<string>());
+        }
+    }
+}
---
+++ AirTrafficMonitor.Test/Integration/Decoder_Track.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirTrafficMonitor.Implementation;
+using AirTrafficMonitor.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TransponderReceiver;
+
+namespace AirTrafficMonitor.Test.Integration
+{
+    [TestFixture]
+    class Decoder_Track
+    {
+        private Decoder decoder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            decoder = new Decoder();
+        }
+
+        [TestCase("ABC123", "10000", "10000", "1000", "20000101101010999")]
+        public void DecodeTransData_IsInAirspace_CalculateVelocityIsCalled(string tag, string x, string y,
+            string altitude, string dateTime)
+        {
+            List<string> list = new List<string>() { tag + ';' + x + ';' + y + ';' + altitude + ';' + dateTime };
+            decoder.DecodeTransData(new object(), new RawTransponderDataEventArgs(list));
+
+            List<string> list2 = new List<string>() { tag + ';' + "12000" + ';' + y + ';' + altitude + ';' + "20000101101020999" };
+            decoder.DecodeTransData(new object(), new RawTransponderDataEventArgs(list2));
+
+            //decoder.Track = new Track(){500, 270, "XX1234", DateTime.Now, 50, 10000, 10000};
+
+            Assert.That(Math.Round(decoder.Track.Velocity, 1), Is.EqualTo(200));
+        }
+
+        [TestCase("ABC123", "10000", "10000", "1000", "20000101101010999")]
+        public void DecodeTransData_IsInAirspace_CalculateCourseIsCalled(string tag, string x, string y,
+            string altitude, string dateTime)
+        {
+            List<string> list = new List<string>() { tag + ';' + x + ';' + y + ';' + altitude + ';' + dateTime };
+            decoder.DecodeTransData(new object(), new RawTransponderDataEventArgs(list));
+
+            List<string> list2 = new List<string>() { tag + ';' + "12000" + ';' + y + ';' + altitude + ';' + "20000101101020999" };
+            decoder.DecodeTransData(new object(), new RawTransponderDataEventArgs(list2));
+
+            Assert.That(decoder.Track.Course, Is.EqualTo(270));
+        }
+    }
+}
